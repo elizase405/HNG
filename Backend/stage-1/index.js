cea64d9a90ff6135e99a2ce1ae798b96730b2f29
Elizabeth <elizase405@gmail.com>
2023-09-10 14:33:34 +0100

    Update / output

--- Backend/stage-1/index.js
+++ Backend/stage-1/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.get("/", (req, res)=>{
-	res.send("Go to /api?slack_name=example_name&track=backend");
+	res.send("Go to https://example.com/api?slack_name=example_name&track=backend");
 });
 
 app.get("/api", (req, res)=>{
